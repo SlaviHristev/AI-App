1611529b11708e12a7bd46f97e8eaa98ba0d86d3
SlaviHristev <Slavihristev97@gmail.com>
2024-09-29 12:21:05 +0300

    Started designing the home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom'
 import './home.css'
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+        <img src="/orbital.png" alt="" className='orbital'/>
+        <div className="left">
+            <h1>SLAVI AI</h1>
+            <h2>Supercharge your creativity and productivity!</h2>
+            <h3>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, unde.
+            </h3>
+            <Link to="/dashboard">Get Started</Link>
+        </div>
+        <div className="right">
+
+        </div>
+    </div>
   )
 }
 
