27d319cf0985be2b2b5320c40b71b02880e8b965
SlaviHristev <Slavihristev97@gmail.com>
2024-09-30 18:27:17 +0300

    Added text animation using react-type-animaton

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,13 @@
 import { Link } from 'react-router-dom'
 import './home.css'
+import { TypeAnimation } from 'react-type-animation'
+import { useState } from 'react'
+
 
 const Home = () => {
+
+  const [typingStatus, setTypingStatus] = useState("human1")
+
   return (
     <div className='home'>
         <img src="/orbital.png" alt="" className='orbital'/>
@@ -19,7 +25,43 @@ const Home = () => {
                 <div className="bg"> </div>
               </div>
                 <img src="/bot.png" alt="" className='bot' />
+                <div className="chat">
+                  <img src={typingStatus === 'human1' ? "/human1.jpeg" :  typingStatus === "human2" ? "/human2.jpeg" : "/bot.png" } alt="" />
+                  <TypeAnimation
+                  sequence={[
+                    "Human: Lorem ipsum dolor sit amet consectetur adipisicing elit!",
+                    2000,() =>{
+                      setTypingStatus("bot")
+                    },
+                    "Bot: Lorem ipsum dolor sit amet consectetur!",
+                    2000,
+                    () =>{
+                      setTypingStatus("human2")
+                    },
+                    "Human2: Lorem ipsum adipisicing elit. Ullam, unde."
+                    ,2000,() =>{
+                      setTypingStatus("bot")
+                    },
+                    "Bot: Lorem ipsum dolor sit amet consectetur!",
+                    2000,
+                    () =>{
+                      setTypingStatus("human1")
+                    },
+                  ]}
+                  wrapper='span'
+                  repeat={Infinity}
+                  cursor={true}
+                  omitDeletionAnimation={true}/>
+                </div>
             </div>
+        </div>
+        <div className="terms">
+          <img src="/logo.png" alt="" />
+          <div className="links">
+            <Link to="/">Terms of Service</Link>
+            <span>|</span>
+            <Link to="/">Privacy Policy</Link>
+          </div>
         </div>
     </div>
   )
