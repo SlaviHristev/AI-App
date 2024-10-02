e217e37419092a102dbca611c6b1a5b4e20839b0
SlaviHristev <Slavihristev97@gmail.com>
2024-10-02 18:18:38 +0300

    Started working on the chat page

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -2,7 +2,24 @@ import './chat.css'
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test1</div>
+          <div className="message user">Test2</div>
+          <div className="message">Test1</div>
+          <div className="message user">Test2</div>
+          <div className="message">Test1</div>
+          <div className="message user">Test2</div>
+          <div className="message">Test1</div>
+          <div className="message user">Test2</div>
+          <div className="message">Test1</div>
+          <div className="message user">Test2</div>
+          <div className="message">Test1</div>
+          <div className="message user">Test2</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
