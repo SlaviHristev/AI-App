c2cf26bd29de2eb87a1426e111ef7cf4e412432a
SlaviHristev <Slavihristev97@gmail.com>
2024-10-06 11:42:58 +0300

    Finished the uploading image logic

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import ImageKit from "imagekit";
 
 
@@ -6,18 +7,23 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL
+    }))
+
 const imagekit = new ImageKit({
-    urlEndpoint:  process.env.IMAGE_KIT_ENDPOINT,
+    urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
     publicKey: process.env.IMAGE_KIT_PUBLIC_KEY,
     privateKey: process.env.IMAGE_KIT_PRIVATE_KEY
 })
 
-app.get('/api/upload', (req,res) =>{
+app.get('/api/upload', (req, res) => {
     const result = imagekit.getAuthenticationParameters();
     res.send(result);
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log("Server is running..");
-    
+
 })
