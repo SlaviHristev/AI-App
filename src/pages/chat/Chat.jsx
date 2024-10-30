addc8ceaf2c7d744e5ce1ba34504751f0fb1b12a
SlaviHristev <Slavihristev97@gmail.com>
2024-10-30 17:52:51 +0200

    Implemented fetching info for single page and using react query mutation

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,29 +1,52 @@
-import { useRef } from 'react'
+
 import './chat.css'
-import { useEffect } from 'react';
 import NewPrompt from '../../components/newPrompt/NewPrompt';
+import { useQuery } from '@tanstack/react-query';
+import Markdown from 'react-markdown';
+import { IKImage } from 'imagekitio-react';
 
 const Chat = () => {
+  const path = useLocation().pathname;
+  const chatId = path.split("/").pop();
 
-  
+  const { isPending, error, data } = useQuery({
+    queryKey: ['chat', chatId],
+    queryFn: () =>
+      fetch(`${import.meta.env.VITE_API_URL}/api/chats/${chatId}`, {
+        credentials: "include"
+      }).then((res) => {
+        res.json()
+      })
+  })
 
   return (
     <div className='chatPage'>
       <div className="wrapper">
         <div className="chat">
           <div className="message">Test1</div>
-          <div className="message user">Test2</div>
-          <div className="message">Test1</div>
-          <div className="message user">Test2</div>
-          <div className="message">Test1</div>
-          <div className="message user">Test2</div>
-          <div className="message">Test1</div>
-          <div className="message user">Test2</div>
-          <div className="message">Test1</div>
-          <div className="message user">Test2</div>
-          <div className="message">Test1</div>
-          <div className="message user">Test2</div>
-          <NewPrompt/>
+          {isPending ? "Loading..." :
+            error ? "Something went wrong..." :
+              data?.history.map((message, i) => (
+                <>
+                  {message.img && (
+                    <IKImage
+                      urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+                      path={message.img}
+                      height="300"
+                      width="400"
+                      transformation={[{ height: 300, width: 400 }]}
+                      loading='lazy'
+                      lqip={{active:true, quality: 20}}
+                    />
+                  )}
+                  <div className={message.role === "user" ? "message user" : "message"} key={i}>
+                    <Markdown>{message.parts[0].text}</Markdown>
+                  </div>
+                </>
+
+              ))}
+
+          <NewPrompt />
         </div>
       </div>
     </div>
